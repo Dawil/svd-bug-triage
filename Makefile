89d845b02315a31055e06a2018f234e9c6d66252
David Wilcox <dave@davidgwilcox.com>
2016-11-23 20:13:13 +1100

    done?

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
 assignee_keywords.csv:
 	python main.py get_assignee_keywords
+
+foo: assignee_keywords.csv
+	python main.py load_assignee_keywords
+.PHONY: foo
+
+save: assignee_keywords.csv
+	python main.py save_matrix
+.PHONY: save
+
+interactive: assignee_keywords.csv
+	python main.py interactive
+.PHONY: interactive
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import argparse
 import csv
+import numpy as np
 
 def open_csv(filename, as_dict=True):
-    contents = open(filename).read().split("\n")
+    contents = open(filename).read().strip().split("\n")
     if as_dict:
         print('foo')
         reader = csv.DictReader(contents)
@@ -17,12 +18,73 @@ def get_assignee_keywords():
     for bug in bugs:
         if bug['Keywords']:
             assignee_keywords[bug['Assignee']] = assignee_keywords.get(bug['Assignee'], [])
-            assignee_keywords[bug['Assignee']].append(bug['Keywords'])
+            assignee_keywords[bug['Assignee']] += map(
+                lambda keyword: keyword.strip(),
+                bug['Keywords'].split(',')
+            )
 
     with open('assignee_keywords.csv', 'w+') as f:
         writer = csv.writer(f)
         for assignee, keywords in assignee_keywords.items():
             writer.writerow([assignee] + keywords)
+
+def load_assignee_keywords():
+    assignee_keywords = open_csv('assignee_keywords.csv', as_dict=False)
+    ak_dict = {}
+    unique_keywords = set()
+    for row in assignee_keywords:
+        ak_dict[row[0]] = {}
+        for keyword in row[1:]:
+            unique_keywords.add(keyword)
+            ak_dict[row[0]][keyword] = ak_dict[row[0]].get(keyword, 0) + 1
+    import pdb
+    pdb.set_trace()
+    keywords = sorted(list(unique_keywords))
+    assignees = sorted(list(filter(
+        lambda assignee: sum(ak_dict[assignee].values()) > 5,
+        ak_dict.keys()
+    )))
+
+    matrix = [
+        [
+            ak_dict[assignee].get(keyword, 0)
+            for keyword in keywords
+        ]
+        for assignee in assignees
+    ]
+    return (assignees, keywords, matrix)
+
+def save_matrix():
+    assignees, keywords, matrix, U, s, V = compute_svd()
+    with open('matrix.csv', 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerows(matrix)
+    with open('assignees.txt', 'w+') as f:
+        f.write("\n".join(assignees))
+    with open('keywords.txt', 'w+') as f:
+        f.write("\n".join(keywords))
+    with open('U.csv', 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerows(U)
+    with open('V.csv', 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerows(V)
+    with open('s.txt', 'w+') as f:
+        f.write("\n".join(map(str, s)))
+
+def compute_svd():
+    assignees, keywords, matrix = load_assignee_keywords()
+    U, s, V = np.linalg.svd(matrix)
+    return (
+        assignees, keywords, matrix,
+        U, s, V
+    )
+
+def interactive():
+    assignees, keywords, matrix, U, s, V = compute_svd()
+    import pdb
+    pdb.set_trace()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
